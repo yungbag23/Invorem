6b6b7b798e451ce3d7bfd7b5aede336312706487
yungbag23 <67779679+yungbag23@users.noreply.github.com>
2022-10-26 12:24:39 +0300

    invorem
    invorem

---
+++ invorem/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        dots: false,
+        margin: 48,
+        loop: true
+    });
+  });
+
+/* Video */
+const videoBtn = document.querySelector('#video-btn');
+const videoPicture = document.querySelector('.video__picture');
+const videoWrapper = document.querySelector('.video');
+const video = document.querySelector('#video-file');
+
+
+videoWrapper.addEventListener('click', function () {
+    if(video.paused) {
+    videoPicture.classList.add('none');
+    videoWrapper.classList.remove('video-overlay');
+     videoBtn.classList.add('none');
+    video.play()
+    } else {
+    videoBtn.classList.remove('none');
+    videoWrapper.classList.add('video-overlay');
+    video.pause()
+    }
+})
